e6aff36bc38840dce2e478a504f65bba465e706d
Mihail Marian <mihailmarian12@gmail.com>
2021-01-05 15:08:14 +0000

    made connection to firestore work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import "./App.scss";
 import fire from "./resources/fire.js";
+import { useState, useEffect } from "react";
 
 function App() {
-  console.log(fire);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fire
+      .firestore()
+      .collection("testing")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, "=>", doc.data());
+        });
+      });
+  }, []);
+
   return <div className="App"></div>;
 }
 

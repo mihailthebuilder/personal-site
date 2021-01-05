23e32ee6e3ab19f62f1df7004b9c6098cd26116c
Mihail Marian <mihailmarian12@gmail.com>
2021-01-05 11:15:36 +0000

    imported firestore and cloud storage capabilities

--- src/resources/fire.js
+++ src/resources/fire.js
@@ -1,5 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/analytics";
+import "firebase/firestore";
+import "firebase/storage";
 
 const config = {
   apiKey: "AIzaSyDeGIjqo1s7Y-Vsel1AXPTmZKYQN-U3BWQ",

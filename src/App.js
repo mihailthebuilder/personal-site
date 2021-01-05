2a006d6b8022f421f1ca8c0e96d5b98696431dca
Mihail Marian <mihailmarian12@gmail.com>
2021-01-05 11:12:44 +0000

    added firebase app & analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.scss";
+import fire from "./resources/fire.js";
 
 function App() {
+  console.log(fire);
   return <div className="App"></div>;
 }
 
---
+++ src/resources/fire.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+
+const config = {
+  apiKey: "AIzaSyDeGIjqo1s7Y-Vsel1AXPTmZKYQN-U3BWQ",
+  authDomain: "personal-site-62ba5.firebaseapp.com",
+  projectId: "personal-site-62ba5",
+  storageBucket: "personal-site-62ba5.appspot.com",
+  messagingSenderId: "500725287358",
+  appId: "1:500725287358:web:e7d184da0d99ac0933d1ef",
+  measurementId: "G-9F9D3GCV65",
+};
+
+const fire = firebase.initializeApp(config);
+firebase.analytics();
+
+export default fire;

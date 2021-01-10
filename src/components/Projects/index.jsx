9e913c6660709df687b50c58697b1b38cc98c281
Mihail Marian <mihailmarian12@gmail.com>
2021-01-10 12:26:04 +0000

    started working on connecting firebase storage

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -5,18 +5,43 @@ import "./Projects.scss";
 
 const Projects = ({ typewriterText, startAnimations }) => {
   const [projectIndex, setProjectIndex] = useState(0);
+  const [projectList, setProjectList] = useState([]);
+  const [focusProject, setFocusProject] = useState({
+    id: "",
+    image_src: "",
+    description: "",
+    title: "",
+    website_link: "",
+    github_link: "",
+  });
 
   useEffect(() => {
     fire
       .firestore()
-      .collection("testing")
+      .collection("projects")
+      .orderBy("publication_date", "desc")
       .get()
       .then((querySnapshot) => {
+        let newProjectList = [];
         querySnapshot.forEach((doc) => {
-          console.log(doc.id, "=>", doc.data());
+          const project = { id: doc.id, ...doc.data() };
+          newProjectList = newProjectList.concat(project);
         });
+        setProjectList(newProjectList);
       });
   }, []);
+
+  useEffect(() => {
+    if (projectList.length > 0) {
+      const project = projectList[projectIndex];
+      const imageSrc = fire.storage().ref(`img/${project.id}.png`);
+      setFocusProject({ image_src: imageSrc, ...project });
+    }
+  }, [projectIndex, projectList]);
+
+  useEffect(() => {
+    console.log(focusProject);
+  }, [focusProject]);
 
   return (
     <section id="projects" className="section-padding">
@@ -24,7 +49,9 @@ const Projects = ({ typewriterText, startAnimations }) => {
         {typewriterText}
         <span className="typewriter-cursor">|</span>
       </div>
-      <div className="project-chooser"></div>
+      <div className="project-chooser">
+        <img src={focusProject.image_src} alt="Project" />
+      </div>
     </section>
   );
 };

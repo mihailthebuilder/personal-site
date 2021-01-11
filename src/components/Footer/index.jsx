3ef07eb9fb3d106494cdc37e459d468b8f0e310a
Mihail Marian <mihailmarian12@gmail.com>
2021-01-11 16:20:40 +0000

    set up footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,6 +4,9 @@ const Footer = () => {
   return (
     <footer>
       <div>&#169; Mihail Marian</div>
+      <div className="ps-footer">
+        (P.S. I don't actually know how this copyright disclaimer helps me...)
+      </div>
     </footer>
   );
 };

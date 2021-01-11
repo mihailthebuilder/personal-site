913d7582fdd33bd944311c782f6c8a8c14e48cb7
Mihail Marian <mihailmarian12@gmail.com>
2021-01-11 16:06:18 +0000

    simplified font stylings

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from "./components/Landing";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         typewriterText="contact."
         ChildComponent={Contact}
       />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>&#169; Mihail Marian</div>
+    </footer>
+  );
+};
+
+export default Footer;
